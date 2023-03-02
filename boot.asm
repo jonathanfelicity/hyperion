4364dd31c5236c4503caa3252003352b2b72c3c4
Jonathan Felicity <jonathanfelicity@mail.com>
2023-03-02 17:42:13 +0100

    improve boot loader

--- boot.asm
+++ boot.asm
@@ -1,7 +1,16 @@
-ORG 0x7c00
+ORG 0
 BITS 16
 
+jmp 0x7c0:start
 start:
+    cli ; clear interrups
+    mov ax, 0x7c0
+    mov ds, ax
+    mov es, ax
+    mov ax, 0x00
+    mov ss, ax
+    mov sp, 0x7c00
+    sti ; enables interrups
     mov si, message
     call print
     jmp $
--- exec.sh
+++ exec.sh
@@ -1,3 +1,4 @@
+rm ./boot.bin
 nasm -f bin ./boot.asm -o ./boot.bin
 
 qemu-system-x86_64 -hda ./boot.bin
